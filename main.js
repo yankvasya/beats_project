0de097f30009cc93cd2887b8e5c2a04f7e68beab
yankvasya <yankvasya@gmail.com>
2021-07-31 11:58:47 +0300

    Вертикальный аккордеон

--- main.js
+++ main.js
@@ -6,13 +6,54 @@ const hamburgerClose = document.querySelector('.hamburger__close');
 const fixedMenu = document.querySelector('.fixed-menu__link');
 
 hamburger.addEventListener('click', () => {
+  e.preventDefault();
   hamburgerAll.style.display = `flex`;
   body.style.overflowY = `hidden`;
 });
 
-hamburgerClose.addEventListener('click', () => {
+hamburgerClose.addEventListener('click', e => {
+  e.preventDefault();
   hamburgerAll.style.display = `none`;
   body.style.overflowY = `scroll`;
+});
+
+$('.member__drop-link').on('click', e => { // Реализация аккордеона
+  e.preventDefault();
+  let child = $(e.currentTarget).children().eq(2);
+  let elem = $(child).prev();
+
+  if ($(child).hasClass('active')) {
+    $('.active').slideUp();
+    $('.active').removeClass();
+    $(elem).css('transform', 'rotate(-180deg)');
+  } else {
+    $('.member__elem').css('transform', 'rotate(-180deg)');
+    $('.active').slideUp();
+    $('.active').removeClass();
+    $(elem).css('transform', 'rotate(0deg)');
+    $(child).slideToggle();
+    $(child).addClass('active');
+  }
+
+  console.log($(elem));
+});
+
+
+$('.fixed-menu__link').on('click', e => {
+  e.preventDefault();
+});
+
+$('.button').on('click', e => {
+  e.preventDefault();
+});
+
+$('.social__link').on('click', e => {
+  e.preventDefault();
 
 });
 
+
+$('.person').on('click', e => {
+  e.preventDefault();
+
+});
