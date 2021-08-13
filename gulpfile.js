802c84ccd39ba305a64981136097d43b98116c5c
yankvasya <yankvasya@gmail.com>
2021-08-13 14:35:19 +0300

    Почти закончил prod/dev

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ task('copy:html', () => {
 });
 
 task('styles', () => {
-  return src([...STYLES_LIBS, 'src/css/**/*.scss'])
+  return src([...STYLES_LIBS, 'src/css/style.scss'])
     .pipe(gulpif(env === 'dev', sourcemaps.init()))
     .pipe(concat('main.min.scss'))
     .pipe(sassGlob())
@@ -65,7 +65,7 @@ task('server', () => {
 });
 
 task('scripts', () => {
-  return src([...JS_LIBS, 'src/scripts/*.js'])
+  return src([...JS_LIBS, 'src/*.js'])
     .pipe(gulpif(env === 'prod', babel({
       presets: ['@babel/env']
     })))
@@ -77,43 +77,38 @@ task('scripts', () => {
     .pipe(reload({ stream: true }));
 });
 
-task('icons', () => {
-  return src(`${SRC_PATH}/images/icons/*.svg`)
-    .pipe(
-      svgo({
-        plugins: [
-          {
-            removeAttrs: {
-              attrs: "(fill|stroke|width|height|data.*)"
-            }
-          }
-        ]
-      })
-    )
-    .pipe(svgSprite({
-      mode: {
-        symbol: {
-          sprite: "../sprite.svg"
-        }
-      }
-    }))
-    .pipe(dest(`${DIST_PATH}/images/icons`));
+task('copy:icons', () => {
+  return src(`${SRC_PATH}/icons/**/*.svg`).pipe(dest(`${DIST_PATH}/icons`));
+})
+
+task('copy:video', () => {
+  return src(`${SRC_PATH}/video/*`).pipe(dest(`${DIST_PATH}/video`));
+})
+
+task('copy:img', () => {
+  return src(`${SRC_PATH}/img/**/*`).pipe(dest(`${DIST_PATH}/img`));
+})
+
+task('copy:logo', () => {
+  return src(`${SRC_PATH}/logo/*`).pipe(dest(`${DIST_PATH}/logo`));
 })
 
 task('watch', () => {
-  watch(`${SRC_PATH}/styles/**/*.scss`, series('styles'));
+  watch(`${SRC_PATH}/css/**/*.scss`, series('styles'));
   watch(`${SRC_PATH}/*.html`, series('copy:html'));
-  watch(`${SRC_PATH}/scripts/*.js`, series('scripts'));
-  watch(`${SRC_PATH}/images/icons/*.svg`, series('icons'));
+  watch(`${SRC_PATH}/*.js`, series('scripts'));
+  // watch(`${SRC_PATH}/images/icons/*.svg`, series('icons'));
 })
 
 task('default',
   series('clean',
-    parallel('copy:html', 'styles', 'scripts', 'icons'),
+    parallel('copy:html', 'styles', 'scripts', 'copy:icons', 'copy:video', 'copy:img', 'copy:logo'),
     parallel('watch', 'server')
   )
 );
 
 task('build',
-  series('clean', parallel('copy:html', 'styles', 'scripts', 'icons'))
+  series('clean',
+    parallel('copy:html', 'styles', 'scripts', 'copy:icons', 'copy:img', 'copy:video', 'copy:logo')
+  )
 );
