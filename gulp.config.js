8427906101f501337d1b39b43e093359105dfb2c
yankvasya <yankvasya@gmail.com>
2021-08-13 15:18:51 +0300

    DEV/PROD: MISSION COPMLETED

--- gulp.config.js
+++ gulp.config.js
@@ -2,5 +2,5 @@ module.exports = {
   SRC_PATH: 'src',
   DIST_PATH: 'dist',
   STYLES_LIBS: ['node_modules/normalize.css/normalize.css'],
-  JS_LIBS: ['node_modules/jquery/dist/jquery.js'],
+  JS_LIBS: ['node_modules/jquery/dist/jquery.js', 'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js', 'node_modules/bxslider/dist/jquery.bxslider.js'],
 }
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ task('styles', () => {
     .pipe(concat('main.min.scss'))
     .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
-    .pipe(px2rem())
+    // .pipe(px2rem())
     .pipe(
       gulpif(env === 'dev',
         autoprefixer({
