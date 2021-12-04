4493ad723e97f4326af01c8c0e7840e1029fe264
yankvasya <yankvasya@gmail.com>
2021-12-04 21:20:11 +0300

    fixed paths

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   SRC_PATH: 'src',
-  DIST_PATH: 'dist',
+  DIST_PATH: 'docs',
   STYLES_LIBS: ['node_modules/normalize.css/normalize.css'],
   JS_LIBS: ['node_modules/jquery/dist/jquery.js', 'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js', 'node_modules/bxslider/dist/jquery.bxslider.js'],
 }
