90e03a90459d38e8e35ef9066e12802bbd8e3fba
yankvasya <yankvasya@gmail.com>
2021-12-04 21:21:40 +0300

    gh-pages

--- gulp.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  SRC_PATH: 'src',
-  DIST_PATH: 'docs',
-  STYLES_LIBS: ['node_modules/normalize.css/normalize.css'],
-  JS_LIBS: ['node_modules/jquery/dist/jquery.js', 'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js', 'node_modules/bxslider/dist/jquery.bxslider.js'],
-}
